588e20c266863bac82cb6af4b8d1e2ebd01aae8d
AlvaroHH2 <83060352+AlvaroHH2@users.noreply.github.com>
2021-10-26 10:20:30 +0200

    Add files via upload

--- encapsulamiento.py
+++ encapsulamiento.py
@@ -1,48 +1,56 @@
-class alumno(object):
-   def __init__(self,nombre,edad):
-        self.nombre=nombre
-        self.edad=edad
+class alumno(object):  # Declaramos la clase principal Perros
+    def __init__(self, nombre, edad):  # Definimos los parámetros
+        self.__nombre = nombre  # Declaramos los atributos
+        self.__edad = edad
 
-   @property
-   def nombre(self):  # Definimos el método para obtener el nombre
+    @property
+    def nombre(self):  # Definimos el método para obtener el nombre
         "Documentación del método nombre bla bla"  # Doc del método
-        return self.__nombre
+        return self.__nombre  # Aquí simplemente estamos retornando el atributo privado
 
-   @nombre.setter
-   def nombre(self, nuevo):
-        self.__nombre = nuevo
-        print(self.__nombre)  # Aquí vuelvo a pedir que retorne el atributo para confirmar
-
+    # Hasta aquí definimos los métodos para obtener los atributos ocultos o privados getter.
+    # Ahora vamos a utilizar setter y deleter para modificarlos
+    @nombre.setter  # Propiedad SETTER
+    def nombre(self, nuevo):
         print("Modificando nombre..")
-
+        self.__nombre = nuevo
 
         print("El nombre se ha modificado por")
-
         print(self.__nombre)
+        print("Con una edad de ")
+        print(self.__edad)# Aquí vuelvo a pedir que retorne el atributo para confirmar
+
+    @nombre.deleter  # Propiedad DELETER
+    def nombre(self):
+        print("Borrando nombre..")
+        del self.__nombre
+
+    @property
+    def edad(self):  # Definimos el método para obtener la edad #Automáticamente GETTER
+        return self.__edad  # Aquí simplemente estamos retornando el atributo privado
+
+    @edad.setter
+    def edad(self, nuevaedad):
+
+        self.__edad = nuevaedad
+        print("La edad ahora es")
+        print(self.__edad)
+
+    @edad.deleter  # Propiedad DELETER
+    def edad(self):
+        print("Borrando edad..")
+        del self.__edad
 
 
-   @nombre.deleter  # Propiedad DELETER
-   def nombre(self):
-     print("Borrando nombre..")
-     del self.__nombre
+# Instanciamos
+Tomas = alumno('Manuel', 27)
+print(Tomas.nombre)  # Imprimimos el nombre de Tomas. Se hace a través de getter
+# Que en este caso como esta luego de property lo toma como el primer método..
+Tomas.nombre = 'Pepe'  # Cambiamos el atributo nombre que se hace a través de setter
+  # Volvemos a imprimir
+Tomas.edad = 28
+del Tomas.nombre  # Borramos el nombre utilizando deleter
 
 
-   @property
-   def edad(self):
-        return self.__edad
 
-   @edad.setter
-   def edad(self, nuevo2):
-         self.__edad = nuevo2
-         print(self.__edad)
-         print("Modificando edad..")
-
-         print("La edad se ha modificado por")
-         print(self.__edad)  # Aquí vuelvo a pedir que retorne el atributo para confirmar
-
-alumnorec=alumno("Manuel",22)
-alumnorec.nombre='Manolito'
-alumnorec.edad=20
-del alumnorec.nombre
-#print(alumnorec.nombre)
 
